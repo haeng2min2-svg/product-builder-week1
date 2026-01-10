d10005ea2f853153b683289b471f4a06248406b2
haeng2min2 <haeng2min2@gmail.com>
2026-01-10 15:30:39 +0000

    feat: add dark mode and light mode toggle

--- main.js
+++ main.js
@@ -54,3 +54,27 @@ document.getElementById('generate-btn').addEventListener('click', () => {
         lottoNumbersContainer.appendChild(lottoBall);
     }
 });
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.body.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.body.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.body.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
